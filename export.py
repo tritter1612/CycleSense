0c6efca5664e4e02e340d06cf4d6f2b39ff2c69d
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2021-06-01 09:33:04 +0200

    moved linear interpolation to preprocess.py file

--- export.py
+++ export.py
@@ -6,7 +6,6 @@ from io import StringIO
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
-import datetime as dt
 
 def export(data_dir, target_dir, info, target_region=None):
 
@@ -163,16 +162,6 @@ def export(data_dir, target_dir, info, target_region=None):
                             # drop unnecessary columns
                             df.drop(['obsDistanceLeft1', 'obsDistanceLeft2', 'obsDistanceRight1', 'obsDistanceRight2',
                                      'obsClosePassEvent'], 1, inplace=True)
-
-                            # set timeStamp col as pandas datetime index
-                            df['timeStamp'] = df['timeStamp'].apply(lambda x: dt.datetime.utcfromtimestamp(x / 1000).strftime('%d.%m.%Y %H:%M:%S,%f'))
-                            df['timeStamp'] = pd.to_datetime(df['timeStamp'])
-                            df = df.set_index(pd.DatetimeIndex(df['timeStamp']))
-
-                            # linear interpolation of missing values
-                            df['lat'].interpolate(method='time', inplace=True)
-                            df['lon'].interpolate(method='time', inplace=True)
-                            df['acc'].interpolate(method='time', inplace=True)
 
                             if int(file[-1]) in [3, 6, 9]:
                                 # test set
---
+++ preprocess.py
@@ -0,0 +1,38 @@
+import os
+import glob
+
+import pandas as pd
+from tqdm.auto import tqdm
+import datetime as dt
+
+
+def linear_interpolate(dir):
+    for split in ['train', 'test']:
+
+        for i, subdir in tqdm(enumerate(os.listdir(os.path.join(dir, split))),
+                              desc='apply linear interpolation on {} data'.format(split),
+                              total=len(glob.glob(os.path.join(dir, split, '*')))):
+            if not subdir.startswith('.'):
+                region = subdir
+
+                for j, file in tqdm(enumerate(os.listdir(os.path.join(dir, split, subdir))), disable=True,
+                                    desc='loop over rides in {}'.format(region),
+                                    total=len(glob.glob(os.path.join(dir, split, subdir, 'VM2_*')))):
+                    df = pd.read_csv(os.path.join(dir, split, subdir, file))
+
+                    # set timeStamp col as pandas datetime index
+                    df['timeStamp'] = df['timeStamp'].apply(
+                        lambda x: dt.datetime.utcfromtimestamp(x / 1000).strftime('%d.%m.%Y %H:%M:%S,%f'))
+                    df['timeStamp'] = pd.to_datetime(df['timeStamp'])
+                    df = df.set_index(pd.DatetimeIndex(df['timeStamp']))
+
+                    # linear interpolation of missing values
+                    df['lat'].interpolate(method='time', inplace=True)
+                    df['lon'].interpolate(method='time', inplace=True)
+                    df['acc'].interpolate(method='time', inplace=True)
+
+                    df.to_csv(os.path.join(dir, split, subdir, file), ',', index=False)
+
+
+def preprocess(dir):
+    linear_interpolate(dir)
