f4a631e245176b3e01de3293ccd6a622e2864afc
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2021-06-01 09:36:27 +0200

    added scale method for MinMaxScaling

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,11 @@ import glob
 
 import pandas as pd
 from tqdm.auto import tqdm
+from sklearn.preprocessing import MinMaxScaler
 import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def linear_interpolate(dir):
@@ -53,6 +57,48 @@ def calc_gps_delta(dir):
                     df.to_csv(os.path.join(dir, split, subdir, file), ',', index=False)
 
 
+def scale(dir):
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    acc_scaler = MinMaxScaler(feature_range=(0, 1))
+
+    split = 'train'
+
+    for i, subdir in tqdm(enumerate(os.listdir(os.path.join(dir, split))), desc='fit scaler',
+                          total=len(glob.glob(os.path.join(dir, split, '*')))):
+        if not subdir.startswith('.'):
+            region = subdir
+
+            for j, file in tqdm(enumerate(os.listdir(os.path.join(dir, split, subdir))), disable=True,
+                                desc='loop over rides in {}'.format(region),
+                                total=len(glob.glob(os.path.join(dir, split, subdir, 'VM2_*')))):
+
+                df = pd.read_csv(os.path.join(dir, split, subdir, file))
+
+                if df[['XL', 'YL', 'ZL']].isnull().values.any():
+                    os.remove(os.path.join(dir, split, subdir, file))
+
+                scaler.partial_fit(df[['X', 'Y', 'Z', 'a', 'b', 'c', 'XL', 'YL', 'ZL']])
+                acc_scaler.partial_fit(df[['acc']])
+
+    for split in ['train', 'test']:
+
+        for i, subdir in tqdm(enumerate(os.listdir(os.path.join(dir, split))), desc='scale {} data'.format(split),
+                              total=len(glob.glob(os.path.join(dir, split, '*')))):
+            if not subdir.startswith('.'):
+                region = subdir
+
+                for j, file in tqdm(enumerate(os.listdir(os.path.join(dir, split, subdir))), disable=True,
+                                    desc='loop over rides in {}'.format(region),
+                                    total=len(glob.glob(os.path.join(dir, split, subdir, 'VM2_*')))):
+                    df = pd.read_csv(os.path.join(dir, split, subdir, file))
+                    df[['X', 'Y', 'Z', 'a', 'b', 'c', 'XL', 'YL', 'ZL']] = scaler.transform(
+                        df[['X', 'Y', 'Z', 'a', 'b', 'c', 'XL', 'YL', 'ZL']])
+                    df[['acc']] = acc_scaler.transform(df[['acc']])
+
+                    df.to_csv(os.path.join(dir, split, subdir, file), ',', index=False)
+
+
 def preprocess(dir):
     linear_interpolate(dir)
     calc_gps_delta(dir)
+    scale(dir)
