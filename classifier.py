668ada808e798c94bd9b5db25f17dce32faa2a12
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2021-06-25 09:35:11 +0200

    implemented oversampling

--- classifier.py
+++ classifier.py
@@ -1,8 +1,9 @@
+import glob
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import regularizers
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout, BatchNormalization
+from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPooling1D, Dropout, BatchNormalization, TimeDistributed, \
+    LSTM
 
 
 def pack_features_vector(features, labels):
@@ -17,46 +18,104 @@ class DNN(tf.keras.models.Sequential):
         super().__init__()
 
     def create_model(self):
-
         self.add(Dense(100, activation='relu'))
         self.add(Dense(100, activation='relu'))
         self.add(Dense(100, activation='relu'))
         self.add(Flatten())
-        self.add(Dense(1, activation='softmax'))
+        self.add(Dense(1, activation='sigmoid'))
 
-        optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
         self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 
 def train(dir, checkpoint_dir, target_region=None, batch_size=22):
-    data_train = tf.data.experimental.make_csv_dataset(os.path.join(dir, 'train', target_region, '*.csv'),
-                                                       batch_size=batch_size, label_name='incident',
-                                                       num_parallel_reads=int(batch_size / 22),
-                                                       select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b',
-                                                                       'c', 'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ', 'RC',
-                                                                       'bike', 'childCheckBox', 'trailerCheckBox',
-                                                                       'pLoc', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7',
-                                                                       'i8', 'i9', 'i10', 'scary', 'incident'])
-    data_test = tf.data.experimental.make_csv_dataset(os.path.join(dir, 'test', target_region, '*.csv'),
-                                                      batch_size=batch_size, label_name='incident',
-                                                      num_parallel_reads=int(batch_size / 22),
-                                                      select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c',
-                                                                      'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ', 'RC', 'bike',
-                                                                      'childCheckBox', 'trailerCheckBox', 'pLoc', 'i1',
-                                                                      'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9',
-                                                                      'i10', 'scary', 'incident'])
-    data_val = tf.data.experimental.make_csv_dataset(os.path.join(dir, 'val', target_region, '*.csv'),
-                                                     batch_size=batch_size,
-                                                     label_name='incident', num_parallel_reads=int(batch_size / 22),
-                                                     select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c',
-                                                                     'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ', 'RC', 'bike',
-                                                                     'childCheckBox', 'trailerCheckBox', 'pLoc', 'i1',
-                                                                     'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9',
-                                                                     'i10', 'scary', 'incident'])
+    pos_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket_incident.csv'))) * 22)
+    neg_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket.csv'))) * 22)
 
-    train_ds = data_train.map(pack_features_vector)
-    val_ds = data_val.map(pack_features_vector)
+    pos_val_counter = float(len(glob.glob(os.path.join(dir, 'val', target_region, '*_bucket_incident.csv'))) * 22)
+    neg_val_counter = float(len(glob.glob(os.path.join(dir, 'val', target_region, '*_bucket.csv'))) * 22)
+
+    # incidents
+    pos_data_train = tf.data.experimental.make_csv_dataset(
+        os.path.join(dir, 'train', target_region, '*_bucket_incident.csv'),
+        batch_size=batch_size, label_name='incident',
+        num_parallel_reads=4,
+        shuffle=False,
+        prefetch_buffer_size=batch_size,
+        num_epochs=np.trunc(neg_train_counter / pos_train_counter),
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b',
+                        'c', 'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ', 'RC',
+                        'bike', 'childCheckBox', 'trailerCheckBox',
+                        'pLoc', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7',
+                        'i8', 'i9', 'i10', 'scary', 'incident'])
+
+    # non-incidents
+    neg_data_train = tf.data.experimental.make_csv_dataset(
+        os.path.join(dir, 'train', target_region, '*_bucket.csv'),
+        batch_size=batch_size, label_name='incident',
+        num_parallel_reads=4,
+        shuffle=False,
+        prefetch_buffer_size=batch_size,
+        num_epochs=1,
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b',
+                        'c', 'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ',
+                        'RC',
+                        'bike', 'childCheckBox', 'trailerCheckBox',
+                        'pLoc', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6',
+                        'i7',
+                        'i8', 'i9', 'i10', 'scary', 'incident'])
+
+    # incidents
+    pos_data_val = tf.data.experimental.make_csv_dataset(
+        os.path.join(dir, 'val', target_region, '*_bucket_incident.csv'),
+        batch_size=batch_size, label_name='incident',
+        num_parallel_reads=4,
+        shuffle=False,
+        prefetch_buffer_size=batch_size,
+        num_epochs=np.trunc(neg_val_counter / pos_val_counter),
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b',
+                        'c', 'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ', 'RC',
+                        'bike', 'childCheckBox', 'trailerCheckBox',
+                        'pLoc', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7',
+                        'i8', 'i9', 'i10', 'scary', 'incident'])
+
+    # non-incidents
+    neg_data_val = tf.data.experimental.make_csv_dataset(
+        os.path.join(dir, 'val', target_region, '*_bucket.csv'),
+        batch_size=batch_size, label_name='incident',
+        num_parallel_reads=4,
+        shuffle=False,
+        prefetch_buffer_size=batch_size,
+        num_epochs=1,
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b',
+                        'c', 'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ',
+                        'RC',
+                        'bike', 'childCheckBox', 'trailerCheckBox',
+                        'pLoc', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6',
+                        'i7',
+                        'i8', 'i9', 'i10', 'scary', 'incident'])
+
+    data_test = tf.data.experimental.make_csv_dataset(
+        os.path.join(dir, 'test', target_region, '*.csv'),
+        batch_size=batch_size, label_name='incident',
+        num_parallel_reads=4,
+        shuffle=False,
+        prefetch_buffer_size=batch_size,
+        num_epochs=1,
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c',
+                        'XL', 'YL', 'ZL', 'RX', 'RY', 'RZ', 'RC', 'bike',
+                        'childCheckBox', 'trailerCheckBox', 'pLoc', 'i1',
+                        'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9',
+                        'i10', 'scary', 'incident'])
+
+    pos_train_ds = pos_data_train.map(pack_features_vector)
+    neg_train_ds = neg_data_train.map(pack_features_vector)
+    pos_val_ds = pos_data_val.map(pack_features_vector)
+    neg_val_ds = neg_data_val.map(pack_features_vector)
     test_ds = data_test.map(pack_features_vector)
+
+    train_ds_resampled = tf.data.experimental.sample_from_datasets([pos_train_ds, neg_train_ds], weights=[0.5, 0.5])
+    val_ds_resampled = tf.data.experimental.sample_from_datasets([pos_val_ds, neg_val_ds], weights=[0.5, 0.5])
 
     model = DNN()
     model.create_model()
@@ -84,7 +143,5 @@ def train(dir, checkpoint_dir, target_region=None, batch_size=22):
         patience=5,
         verbose=1)
 
-    # model.summary()
-
-    model.fit(train_ds, validation_data=val_ds, epochs=5, callbacks=[cp_callback, es_callback])
-    model.eval(test_ds)
+    model.fit(train_ds_resampled, validation_data=val_ds_resampled, epochs=250, callbacks=[cp_callback, es_callback])
+    model.evaluate(test_ds)
