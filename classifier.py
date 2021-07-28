3d5432ed9d2b9c521312b04f40184238e3c0cf55
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2021-07-28 18:56:34 +0200

    changed some hyperparameters; added some evaluation metrics after training

--- classifier.py
+++ classifier.py
@@ -1,6 +1,8 @@
 import os
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPooling1D, Dropout, TimeDistributed, LSTM, ConvLSTM2D
+from sklearn.metrics import matthews_corrcoef, confusion_matrix, f1_score, precision_score, recall_score
 
 from data_loader import load_data
 
@@ -40,7 +42,7 @@ class CNN_LSTM_(tf.keras.models.Sequential):
         super().__init__()
 
     def create_model(self):
-        self.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(None, 2, 11, 9))))
+        self.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(None, 2, 11, 8))))
         self.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu')))
         self.add(TimeDistributed(Dropout(0.5)))
         self.add(TimeDistributed(MaxPooling1D(pool_size=2)))
@@ -49,7 +51,7 @@ class CNN_LSTM_(tf.keras.models.Sequential):
         self.add(Dropout(0.5))
         self.add(Dense(100, activation='relu'))
         self.add(Dense(1, activation='sigmoid'))
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
         self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 
@@ -59,12 +61,12 @@ class Conv_LSTM_(tf.keras.models.Sequential):
         super().__init__()
 
     def create_model(self):
-        self.add(ConvLSTM2D(filters=64, kernel_size=(1, 3), activation='relu', input_shape=(2, 1, 11, 9)))
+        self.add(ConvLSTM2D(filters=64, kernel_size=(1, 3), activation='relu', input_shape=(2, 1, 11, 8)))
         self.add(Dropout(0.5))
         self.add(Flatten())
-        self.add(Dense(100, activation='relu'))
+        self.add(Dense(512, activation='relu'))
         self.add(Dense(1, activation='sigmoid'))
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
         self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 
@@ -96,10 +98,13 @@ def train(train_ds, val_ds, test_ds, num_epochs=10, patience=1, checkpoint_dir='
 
     model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, callbacks=[cp_callback, es_callback])
 
+    print()
+    print('Model evaluation on train set after training:')
+    model.evaluate(train_ds)
+
     latest = tf.train.latest_checkpoint(os.path.dirname(checkpoint_dir))
     model.load_weights(latest)
 
-    print('')
     print('Model evaluation on train set:')
     model.evaluate(train_ds)
     print('Model evaluation on val set:')
@@ -107,14 +112,30 @@ def train(train_ds, val_ds, test_ds, num_epochs=10, patience=1, checkpoint_dir='
     print('Model evaluation on test set:')
     model.evaluate(test_ds)
 
+    y_pred = model.predict(test_ds)
+    y_true = np.concatenate([y for x, y in test_ds], axis=0)
+
+    y_pred = np.round(y_pred)[:,0]
+
+    print('Confusion matrix:')
+    print(confusion_matrix(y_true, y_pred))
+    print('F1 score:')
+    print(f1_score(y_true, y_pred))
+    print('Precision score:')
+    print(precision_score(y_true, y_pred))
+    print('Recall score:')
+    print(recall_score(y_true, y_pred))
+    print('Phi score:')
+    print(matthews_corrcoef(y_true, y_pred))
 
 if __name__ == '__main__':
     dir = '../Ride_Data'
     checkpoint_dir = 'checkpoints/cnn/training'
     target_region = 'Berlin'
-    batch_size = (2 ** 10) * 22
+    bucket_size = 100
+    batch_size = (2 ** 10) * bucket_size
     num_epochs = 100
-    patience = 5
+    patience = 10
 
     train_ds, val_ds, test_ds = load_data(dir, target_region, batch_size, modeln='CNNLSTM')
     train(train_ds, val_ds, test_ds, num_epochs, patience, checkpoint_dir)
