switched from oversampling to loss function weighting; added some metrics; bugfix: bucket files were not read in order, now they are sorted and read in alphabetical order

classifier.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
+from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPooling1D, Dropout, TimeDistributed, LSTM, ConvLSTM2D
-from sklearn.metrics import matthews_corrcoef, confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import matthews_corrcoef, confusion_matrix, f1_score, precision_score, recall_score, roc_auc_score
 
 from data_loader import load_data
 
@@ -42,7 +43,7 @@ class CNN_LSTM_(tf.keras.models.Sequential):
         super().__init__()
 
     def create_model(self):
-        self.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(None, 2, 11, 8))))
+        self.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(None, 4, 25, 8))))
         self.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu')))
         self.add(TimeDistributed(Dropout(0.5)))
         self.add(TimeDistributed(MaxPooling1D(pool_size=2)))
@@ -61,7 +62,7 @@ class Conv_LSTM_(tf.keras.models.Sequential):
         super().__init__()
 
     def create_model(self):
-        self.add(ConvLSTM2D(filters=64, kernel_size=(1, 3), activation='relu', input_shape=(2, 1, 11, 8)))
+        self.add(ConvLSTM2D(filters=64, kernel_size=(1, 3), activation='relu', input_shape=(4, 1, 25, 8)))
         self.add(Dropout(0.5))
         self.add(Flatten())
         self.add(Dense(512, activation='relu'))
@@ -70,7 +71,7 @@ class Conv_LSTM_(tf.keras.models.Sequential):
         self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 
-def train(train_ds, val_ds, test_ds, num_epochs=10, patience=1, checkpoint_dir='checkpoints/cnn/training'):
+def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, checkpoint_dir='checkpoints/cnn/training'):
     model = CNN_LSTM_()
     model.create_model()
 
@@ -96,7 +97,11 @@ def train(train_ds, val_ds, test_ds, num_epochs=10, patience=1, checkpoint_dir='
         patience=patience,
         verbose=1)
 
-    model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, callbacks=[cp_callback, es_callback])
+    # Define the Keras TensorBoard callback.
+    tb_logdir = 'tb_logs/fit/' + datetime.now().strftime('%Y%m%d-%H%M%S')
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_logdir)
+
+    model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, callbacks=[cp_callback, es_callback, tensorboard_callback], class_weight=class_weight)
 
     print()
     print('Model evaluation on train set after training:')
@@ -127,6 +132,8 @@ def train(train_ds, val_ds, test_ds, num_epochs=10, patience=1, checkpoint_dir='
     print(recall_score(y_true, y_pred))
     print('Phi score:')
     print(matthews_corrcoef(y_true, y_pred))
+    print('roc auc score:')
+    print(roc_auc_score(y_true, y_pred))
 
 if __name__ == '__main__':
     dir = '../Ride_Data'
@@ -137,5 +144,5 @@ if __name__ == '__main__':
     num_epochs = 100
     patience = 10
 
-    train_ds, val_ds, test_ds = load_data(dir, target_region, batch_size, modeln='CNNLSTM')
-    train(train_ds, val_ds, test_ds, num_epochs, patience, checkpoint_dir)
+    train_ds, val_ds, test_ds, class_weight = load_data(dir, target_region, batch_size, modeln='CNNLSTM')
+    train(train_ds, val_ds, test_ds, class_weight, num_epochs, patience, checkpoint_dir)

data_loader.py
@@ -12,88 +12,65 @@ def pack_features_vector(features, labels):
     features = tf.stack(list(features.values()), axis=1)
 
     if model == 'LSTM':
-        features = tf.reshape(features, (-1, 22, 9))
-        labels = tf.reshape(labels, (-1, 22,))
+        features = tf.reshape(features, (-1, 100, 8))
+        labels = tf.reshape(labels, (-1, 100,))
         labels = tf.reduce_mean(labels, axis=1)
 
     if model == 'CNNLSTM':
-        features = tf.reshape(features, (-1, 2, 11, 9))
-        labels = tf.reshape(labels, (-1, 22,))
+        features = tf.reshape(features, (-1, 4, 25, 8))
+        labels = tf.reshape(labels, (-1, 100,))
         labels = tf.reduce_mean(labels, axis=1)
 
     if model == 'ConvLSTM':
-        features = tf.reshape(features, (-1, 2, 1, 11, 9))
-        labels = tf.reshape(labels, (-1, 22,))
+        features = tf.reshape(features, (-1, 4, 1, 25, 9))
+        labels = tf.reshape(labels, (-1, 100,))
         labels = tf.reduce_mean(labels, axis=1)
 
     return features, labels
 
 
-def load_data(dir, target_region, batch_size=22, modeln='DNN'):
+def load_data(dir, target_region, batch_size=44, modeln='DNN'):
     global model
     model = modeln
 
-    pos_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket_incident.csv'))) * 22)
-    neg_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket.csv'))) * 22)
+    pos_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket_incident.csv'))))
+    neg_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket.csv'))))
 
-    pos_val_counter = float(len(glob.glob(os.path.join(dir, 'val', target_region, '*_bucket_incident.csv'))) * 22)
-    neg_val_counter = float(len(glob.glob(os.path.join(dir, 'val', target_region, '*_bucket.csv'))) * 22)
+    weight_for_0 = (1 / neg_train_counter) * ((pos_train_counter + neg_train_counter) / 2.0)
+    weight_for_1 = (1 / pos_train_counter) * ((pos_train_counter + neg_train_counter) / 2.0)
 
-    # incidents
-    pos_data_train = tf.data.experimental.make_csv_dataset(
-        os.path.join(dir, 'train', target_region, '*_bucket_incident.csv'),
+    class_weight = {0: weight_for_0, 1: weight_for_1}
+
+    train_list = sorted(glob.glob(os.path.join(dir, 'train', target_region, '*.csv')))
+    val_list = sorted(glob.glob(os.path.join(dir, 'val', target_region, '*.csv')))
+    test_list = sorted(glob.glob(os.path.join(dir, 'test', target_region, '*.csv')))
+
+    data_train = tf.data.experimental.make_csv_dataset(
+        train_list,
         batch_size=batch_size, label_name='incident',
-        num_parallel_reads=1,
-        shuffle=False,
-        prefetch_buffer_size=batch_size,
-        num_epochs=np.trunc(neg_train_counter / pos_train_counter),
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c', 'incident'])
-
-    # non-incidents
-    neg_data_train = tf.data.experimental.make_csv_dataset(
-        os.path.join(dir, 'train', target_region, '*_bucket.csv'),
-        batch_size=batch_size, label_name='incident',
-        num_parallel_reads=1,
         shuffle=False,
         prefetch_buffer_size=batch_size,
         num_epochs=1,
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c', 'incident'])
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
 
-    # incidents
-    pos_data_val = tf.data.experimental.make_csv_dataset(
-        os.path.join(dir, 'val', target_region, '*_bucket_incident.csv'),
+    data_val = tf.data.experimental.make_csv_dataset(
+        val_list,
         batch_size=batch_size, label_name='incident',
-        num_parallel_reads=1,
-        shuffle=False,
-        prefetch_buffer_size=batch_size,
-        num_epochs=np.trunc(neg_val_counter / pos_val_counter),
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c', 'incident'])
-
-    # non-incidents
-    neg_data_val = tf.data.experimental.make_csv_dataset(
-        os.path.join(dir, 'val', target_region, '*_bucket.csv'),
-        batch_size=batch_size, label_name='incident',
-        num_parallel_reads=1,
         shuffle=False,
         prefetch_buffer_size=batch_size,
         num_epochs=1,
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c', 'incident'])
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
 
     data_test = tf.data.experimental.make_csv_dataset(
-        os.path.join(dir, 'test', target_region, '*.csv'),
+        test_list,
         batch_size=batch_size, label_name='incident',
         shuffle=False,
         prefetch_buffer_size=batch_size,
         num_epochs=1,
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'acc', 'a', 'b', 'c', 'incident'])
+        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
 
-    pos_train_ds = pos_data_train.map(pack_features_vector)
-    neg_train_ds = neg_data_train.map(pack_features_vector)
-    pos_val_ds = pos_data_val.map(pack_features_vector)
-    neg_val_ds = neg_data_val.map(pack_features_vector)
+    train_ds = data_train.map(pack_features_vector)
+    val_ds = data_val.map(pack_features_vector)
     test_ds = data_test.map(pack_features_vector)
 
-    train_ds_resampled = tf.data.experimental.sample_from_datasets([pos_train_ds, neg_train_ds], weights=[0.5, 0.5])
-    val_ds_resampled = tf.data.experimental.sample_from_datasets([pos_val_ds, neg_val_ds], weights=[0.5, 0.5])
-
-    return train_ds_resampled, val_ds_resampled, test_ds
+    return train_ds, val_ds, test_ds, class_weight