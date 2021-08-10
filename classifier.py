ff1b6770c95b3a0130c3f6e333d15ea43219a0d6
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2021-08-10 16:43:28 +0200

    added support for fourier transformed data and deepsense model implementation

--- classifier.py
+++ classifier.py
@@ -2,39 +2,11 @@ import os
 import numpy as np
 from datetime import datetime
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPooling1D, Dropout, TimeDistributed, LSTM, ConvLSTM2D
+from tensorflow.keras.layers import Dense, Flatten, Conv1D, Conv2D, Conv3D, RNN, GRUCell, StackedRNNCells, ReLU, \
+    Reshape, BatchNormalization, ReLU, Dropout, MaxPooling1D, Dropout, TimeDistributed, LSTM, ConvLSTM2D
 from sklearn.metrics import matthews_corrcoef, confusion_matrix, f1_score, precision_score, recall_score, roc_auc_score
 
 from data_loader import load_data
-
-
-class DNN_(tf.keras.models.Sequential):
-
-    def __init__(self):
-        super().__init__()
-
-    def create_model(self):
-        self.add(Dense(100, activation='relu'))
-        self.add(Dense(100, activation='relu'))
-        self.add(Dense(100, activation='relu'))
-        self.add(Flatten())
-        self.add(Dense(1, activation='sigmoid'))
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
-
-
-class LSTM_(tf.keras.models.Sequential):
-
-    def __init__(self):
-        super().__init__()
-
-    def create_model(self):
-        self.add(LSTM(100))
-        self.add(Dropout(0.5))
-        self.add(Dense(100, activation='relu'))
-        self.add(Dense(1, activation='sigmoid'))
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 
 class CNN_LSTM_(tf.keras.models.Sequential):
@@ -52,28 +24,165 @@ class CNN_LSTM_(tf.keras.models.Sequential):
         self.add(Dropout(0.5))
         self.add(Dense(100, activation='relu'))
         self.add(Dense(1, activation='sigmoid'))
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-        self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 
-class Conv_LSTM_(tf.keras.models.Sequential):
+class DeepSense(tf.keras.Model):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, fft_window, image_width):
+        super(DeepSense, self).__init__()
 
-    def create_model(self):
-        self.add(ConvLSTM2D(filters=64, kernel_size=(1, 3), activation='relu', input_shape=(4, 1, 25, 8)))
-        self.add(Dropout(0.5))
-        self.add(Flatten())
-        self.add(Dense(512, activation='relu'))
-        self.add(Dense(1, activation='sigmoid'))
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-        self.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+        self.acc_conv1 = Conv3D(64, kernel_size=(3, 3, 3), activation=None, padding='valid',
+                                input_shape=(None, fft_window, image_width, 3, 2))
+        self.acc_batch_norm1 = BatchNormalization()
+        self.acc_act1 = ReLU()
+        self.acc_dropout1 = Dropout(0.5)
+
+        self.acc_conv2 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='same')
+        self.acc_batch_norm2 = BatchNormalization()
+        self.acc_act2 = ReLU()
+        self.acc_dropout2 = Dropout(0.5)
+
+        self.acc_conv3 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='same')
+        self.acc_batch_norm3 = BatchNormalization()
+        self.acc_act3 = ReLU()
+
+        self.gyro_conv1 = Conv3D(64, kernel_size=(3, 3, 3), activation=None, padding='valid',
+                                 input_shape=(None, fft_window, image_width, 3, 2))
+        self.gyro_batch_norm1 = BatchNormalization()
+        self.gyro_act1 = ReLU()
+        self.gyro_dropout1 = Dropout(0.5)
+
+        self.gyro_conv2 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='same')
+        self.gyro_batch_norm2 = BatchNormalization()
+        self.gyro_act2 = ReLU()
+        self.gyro_dropout2 = Dropout(0.5)
+
+        self.gyro_conv3 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='same')
+        self.gyro_batch_norm3 = BatchNormalization()
+        self.gyro_act3 = ReLU()
+
+        self.sensor_dropout = Dropout(0.5)
+
+        self.sensor_conv1 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='SAME')
+        self.sensor_batch_norm1 = BatchNormalization()
+        self.sensor_act1 = ReLU()
+        self.sensor_dropout1 = Dropout(0.5)
+
+        self.sensor_conv2 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='SAME')
+        self.sensor_batch_norm2 = BatchNormalization()
+        self.sensor_act2 = ReLU()
+        self.sensor_dropout2 = Dropout(0.5)
+
+        self.sensor_conv3 = Conv3D(64, kernel_size=(3, 3, 1), activation=None, padding='SAME')
+        self.sensor_batch_norm3 = BatchNormalization()
+        self.sensor_act3 = ReLU()
+        self.sensor_dropout3 = Dropout(0.5)
+
+        self.sensor_reshape = Reshape((18, 6 * 2 * 64))
+
+        self.sensor_gru1 = GRUCell(120, activation=None)
+        self.sensor_gru2 = GRUCell(120, activation=None)
+        self.sensor_stacked_rnn = RNN(StackedRNNCells([self.sensor_gru1, self.sensor_gru2]), return_sequences=True)
+
+        self.sensor_gru1_dropout = GRUCell(120, dropout=0.5, activation=None)
+        self.sensor_gru2_dropout = GRUCell(120, dropout=0.5, activation=None)
+        self.sensor_stacked_rnn_dropout = RNN(StackedRNNCells([self.sensor_gru1_dropout, self.sensor_gru2_dropout]),
+                                              return_sequences=True)
+
+        self.fc = Dense(1, activation='sigmoid')
+
+    def call(self, x, training):
+        # split sensors
+        acc, gyro, gps = tf.split(x, num_or_size_splits=3, axis=3)
+
+        # remove incidents
+        gps = gps[:, :, :, :2]
+
+        # split real and imaginary part of complex accelerometer data
+        acc_real = tf.math.real(acc)
+        acc_imag = tf.math.imag(acc)
+        acc = tf.stack((acc_real, acc_imag), axis=4)
+
+        # split real and imaginary part of complex gyrosensor data
+        gyro_real = tf.math.real(gyro)
+        gyro_imag = tf.math.imag(gyro)
+        gyro = tf.stack((gyro_real, gyro_imag), axis=4)
+
+        # get real part of complex gps data
+        gps = tf.math.real(gps)
+
+        acc = self.acc_conv1(acc)
+        acc = self.acc_batch_norm1(acc)
+        acc = self.acc_act1(acc)
+        acc = self.acc_dropout1(acc) if training else acc
+
+        acc = self.acc_conv2(acc)
+        acc = self.acc_batch_norm2(acc)
+        acc = self.acc_act2(acc)
+        acc = self.acc_dropout2(acc) if training else acc
+
+        acc = self.acc_conv3(acc)
+        acc = self.acc_batch_norm3(acc)
+        acc = self.acc_act3(acc)
+
+        gyro = self.gyro_conv1(gyro)
+        gyro = self.gyro_batch_norm1(gyro)
+        gyro = self.gyro_act1(gyro)
+        gyro = self.gyro_dropout1(gyro) if training else gyro
+
+        gyro = self.gyro_conv2(gyro)
+        gyro = self.gyro_batch_norm2(gyro)
+        gyro = self.gyro_act2(gyro)
+        gyro = self.gyro_dropout2(gyro) if training else gyro
+
+        gyro = self.gyro_conv3(gyro)
+        gyro = self.gyro_batch_norm3(gyro)
+        gyro = self.gyro_act3(gyro)
+
+        sensor = tf.concat([acc[:, :, :, :, :], gyro[:, :, :, :, :]], 3)
+
+        sensor = self.sensor_dropout(sensor)
+
+        sensor = self.sensor_conv1(sensor)
+        sensor = self.sensor_batch_norm1(sensor)
+        sensor = self.sensor_act1(sensor)
+        sensor = self.sensor_dropout1(sensor) if training else sensor
+
+        sensor = self.sensor_conv2(sensor)
+        sensor = self.sensor_batch_norm2(sensor)
+        sensor = self.sensor_act2(sensor)
+        sensor = self.sensor_dropout2(sensor) if training else sensor
+
+        sensor = self.sensor_conv3(sensor)
+        sensor = self.sensor_batch_norm3(sensor)
+        sensor = self.sensor_act3(sensor)
+        sensor = self.sensor_dropout3(sensor) if training else sensor
+
+        sensor = tf.transpose(sensor, perm=(0, 2, 1, 3, 4))
+        sensor = self.sensor_reshape(sensor)
+
+        sensor = self.sensor_stacked_rnn_dropout(sensor) if training else self.sensor_stacked_rnn(sensor)
+
+        sensor = tf.math.reduce_mean(sensor, axis=1, keepdims=False)
+
+        sensor = self.fc(sensor)
+
+        return sensor
 
 
-def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, checkpoint_dir='checkpoints/cnn/training'):
-    model = CNN_LSTM_()
-    model.create_model()
+def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, checkpoint_dir='checkpoints/cnn/training',
+          fourier=True, fft_window=8, image_width=20):
+    if fourier:
+        model = DeepSense(fft_window, image_width)
+
+    else:
+        model = CNN_LSTM_()
+        model.create_model()
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+    auc = tf.keras.metrics.AUC(from_logits=False)
+    model.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
+                  metrics=['accuracy', auc])
 
     latest = tf.train.latest_checkpoint(os.path.dirname(checkpoint_dir))
     try:
@@ -84,7 +193,7 @@ def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, ch
     # Create a callback that saves the model's weights
     cp_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_dir,
-        monitor='val_accuracy',
+        monitor='val_auc',
         verbose=1,
         save_best_only=True,
         mode='max',
@@ -99,9 +208,10 @@ def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, ch
 
     # Define the Keras TensorBoard callback.
     tb_logdir = 'tb_logs/fit/' + datetime.now().strftime('%Y%m%d-%H%M%S')
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_logdir)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_logdir, histogram_freq=1)
 
-    model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, callbacks=[cp_callback, es_callback, tensorboard_callback], class_weight=class_weight)
+    model.fit(train_ds, validation_data=val_ds, epochs=num_epochs,
+              callbacks=[cp_callback, es_callback, tensorboard_callback], class_weight=class_weight)
 
     print()
     print('Model evaluation on train set after training:')
@@ -118,9 +228,10 @@ def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, ch
     model.evaluate(test_ds)
 
     y_pred = model.predict(test_ds)
+
     y_true = np.concatenate([y for x, y in test_ds], axis=0)
 
-    y_pred = np.round(y_pred)[:,0]
+    y_pred = np.round(y_pred)[:, 0]
 
     print('Confusion matrix:')
     print(confusion_matrix(y_true, y_pred))
@@ -132,17 +243,20 @@ def train(train_ds, val_ds, test_ds, class_weight, num_epochs=10, patience=1, ch
     print(recall_score(y_true, y_pred))
     print('Phi score:')
     print(matthews_corrcoef(y_true, y_pred))
-    print('roc auc score:')
-    print(roc_auc_score(y_true, y_pred))
+
 
 if __name__ == '__main__':
     dir = '../Ride_Data'
     checkpoint_dir = 'checkpoints/cnn/training'
     target_region = 'Berlin'
     bucket_size = 100
-    batch_size = (2 ** 10) * bucket_size
+    batch_size = 128
     num_epochs = 100
-    patience = 10
+    patience = 25
+    fourier = True
+    fft_window = 8
+    image_width = 20
 
-    train_ds, val_ds, test_ds, class_weight = load_data(dir, target_region, batch_size, modeln='CNNLSTM')
-    train(train_ds, val_ds, test_ds, class_weight, num_epochs, patience, checkpoint_dir)
+    train_ds, val_ds, test_ds, class_weight = load_data(dir, target_region, batch_size, fourier)
+    train(train_ds, val_ds, test_ds, class_weight, num_epochs, patience, checkpoint_dir, fourier, fft_window,
+          image_width)
--- data_loader.py
+++ data_loader.py
@@ -3,74 +3,70 @@ import glob
 import numpy as np
 import tensorflow as tf
 
-model = None
-
 
 def pack_features_vector(features, labels):
     """Pack the features into a single array."""
 
     features = tf.stack(list(features.values()), axis=1)
-
-    if model == 'LSTM':
-        features = tf.reshape(features, (-1, 100, 8))
-        labels = tf.reshape(labels, (-1, 100,))
-        labels = tf.reduce_mean(labels, axis=1)
-
-    if model == 'CNNLSTM':
-        features = tf.reshape(features, (-1, 4, 25, 8))
-        labels = tf.reshape(labels, (-1, 100,))
-        labels = tf.reduce_mean(labels, axis=1)
-
-    if model == 'ConvLSTM':
-        features = tf.reshape(features, (-1, 4, 1, 25, 9))
-        labels = tf.reshape(labels, (-1, 100,))
-        labels = tf.reduce_mean(labels, axis=1)
+    features = tf.reshape(features, (-1, 4, 25, 8))
+    labels = tf.reshape(labels, (-1, 100,))
+    labels = tf.reduce_mean(labels, axis=1)
 
     return features, labels
 
 
-def load_data(dir, target_region, batch_size=44, modeln='DNN'):
-    global model
-    model = modeln
+def create_ds(dir, target_region, split, batch_size=32, fourier=True, count=False):
 
-    pos_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket_incident.csv'))))
-    neg_train_counter = float(len(glob.glob(os.path.join(dir, 'train', target_region, '*_bucket.csv'))))
+    if fourier:
+
+        with np.load(os.path.join(dir, split, target_region + '.npz')) as data:
+            x = data['arr_0'][:, :, :, :]
+            y = tf.cast(data['arr_0'][:, :, :, 8], tf.dtypes.int32)
+            y = tf.math.reduce_mean(y, axis=1)
+            y = tf.math.reduce_mean(y, axis=1)
+            y = y[:, tf.newaxis]
+            pos_counter = (tf.math.reduce_sum(y)).numpy()
+            neg_counter = y.shape[0] - pos_counter
+
+        ds = tf.data.Dataset.from_tensor_slices((x, y))
+        ds = ds.batch(batch_size)
+
+    else:
+
+        pos_counter = float(len(glob.glob(os.path.join(dir, split, target_region, '*_bucket_incident.csv'))))
+        neg_counter = float(len(glob.glob(os.path.join(dir, split, target_region, '*_bucket.csv'))))
+
+        file_list = sorted(glob.glob(os.path.join(dir, split, target_region, '*.csv')))
+
+        data = tf.data.experimental.make_csv_dataset(
+            file_list, label_name='incident',
+            batch_size=batch_size,
+            shuffle=False,
+            num_epochs=1,
+            select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
+
+        ds = data.map(pack_features_vector)
+
+    if count == True:
+        return ds, pos_counter, neg_counter
+
+    else:
+        return ds
+
+
+def load_data(dir, target_region, batch_size=32, fourier=True):
+
+    train_ds, pos_train_counter, neg_train_counter = create_ds(dir, target_region, 'train', batch_size, fourier, True)
+    val_ds = create_ds(dir, target_region, 'val', batch_size, fourier)
+    test_ds = create_ds(dir, target_region, 'test', batch_size, fourier)
+
+    train_ds = train_ds
+    val_ds = val_ds
+    test_ds = test_ds
 
     weight_for_0 = (1 / neg_train_counter) * ((pos_train_counter + neg_train_counter) / 2.0)
     weight_for_1 = (1 / pos_train_counter) * ((pos_train_counter + neg_train_counter) / 2.0)
 
     class_weight = {0: weight_for_0, 1: weight_for_1}
-
-    train_list = sorted(glob.glob(os.path.join(dir, 'train', target_region, '*.csv')))
-    val_list = sorted(glob.glob(os.path.join(dir, 'val', target_region, '*.csv')))
-    test_list = sorted(glob.glob(os.path.join(dir, 'test', target_region, '*.csv')))
-
-    data_train = tf.data.experimental.make_csv_dataset(
-        train_list,
-        batch_size=batch_size, label_name='incident',
-        shuffle=False,
-        prefetch_buffer_size=batch_size,
-        num_epochs=1,
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
-
-    data_val = tf.data.experimental.make_csv_dataset(
-        val_list,
-        batch_size=batch_size, label_name='incident',
-        shuffle=False,
-        prefetch_buffer_size=batch_size,
-        num_epochs=1,
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
-
-    data_test = tf.data.experimental.make_csv_dataset(
-        test_list,
-        batch_size=batch_size, label_name='incident',
-        shuffle=False,
-        prefetch_buffer_size=batch_size,
-        num_epochs=1,
-        select_columns=['lat', 'lon', 'X', 'Y', 'Z', 'a', 'b', 'c', 'incident'])
-
-    train_ds = data_train.map(pack_features_vector)
-    val_ds = data_val.map(pack_features_vector)
-    test_ds = data_test.map(pack_features_vector)
 
     return train_ds, val_ds, test_ds, class_weight
